37de3e8c2c011d5def293209ca6366d78727dc18
Luca Lambda <luca@lambia.it>
2023-06-12 12:10:59 +0200

    bonus

--- functions.php
+++ functions.php
@@ -1,21 +1,46 @@
 <?php 
 
-function generatePassword($length) {
+function generatePassword($length, $repetition, $subset_azlower, $subset_azupper, $subset_numbers, $subset_symbols) {
     $result = "";
 
     $lowercase = "abcdefghijklmnopqrstuvwxyz";
     $uppercase = strtoupper($lowercase);
     $numbers = "0123456789";
-    $symbol = "!?&%$<>^+-*/()[]{}@#_=";
+    $symbols = "!?&%$<>^+-*/()[]{}@#_=";
 
-    $allowedChar = $lowercase . $uppercase . $numbers . $symbol;
+    $allowedChar = "";
+
+    if($subset_azlower) {
+        $allowedChar .= $lowercase;
+    }
+    if($subset_azupper) {
+        $allowedChar .= $uppercase;
+    }
+    if($subset_numbers) {
+        $allowedChar .= $numbers;
+    }
+    if($subset_symbols) {
+        $allowedChar .= $symbols;
+    }
+
+    if ($allowedChar == "") {
+        $allowedChar = $lowercase . $uppercase . $numbers . $symbols;
+    }
 
     $charNumber = strlen($allowedChar);
 
-    for ($i=0; $i < $length; $i++) { 
+    //Se non abbiamo sufficienti caratteri, la password sarà più breve
+    if($charNumber < $length) {
+        $length = $charNumber;
+    }
+
+    while(strlen($result) < $length) {
         $randoNum = rand(0, $charNumber-1 );
         $currentChar = $allowedChar[$randoNum];
-        $result .= $currentChar;
+        
+        if( $repetition || !str_contains($result, $currentChar) ) {
+            $result .= $currentChar;
+        }
     }
 
     return htmlspecialchars($result);
--- index.php
+++ index.php
@@ -4,13 +4,19 @@ include './functions.php';
 
 if (isset($_GET["pwdlen"])) {
     $pwdLength = $_GET["pwdlen"];
-    
-    $result = generatePassword($pwdLength);
+    $repetition = $_GET["repetition"];
+
+    $subset_azlower = $_GET["subset_azlower"];
+    $subset_azupper = $_GET["subset_azupper"];
+    $subset_numbers = $_GET["subset_numbers"];
+    $subset_symbols = $_GET["subset_symbols"];
+
+    $result = generatePassword($pwdLength, $repetition, $subset_azlower, $subset_azupper, $subset_numbers, $subset_symbols);
 
     session_start();
     $_SESSION["password"] = $result;
 
-    header("Location: ./success.php");
+    // header("Location: ./success.php");
 }
 
 ?>
@@ -34,23 +40,81 @@ if (isset($_GET["pwdlen"])) {
                 <h1>Strong PwdGen</h1>
             </div>
 
-            <?php if($result){ ?>
-            <div class="col-12">
-                <div class="alert alert-primary" role="alert">
-                    <?php echo $result ?>
+            <?php if ($result) { ?>
+                <div class="col-12">
+                    <div class="alert alert-primary" role="alert">
+                        <?php echo $result ?>
+                    </div>
                 </div>
-            </div>
             <?php } ?>
 
             <div class="col-12">
                 <form action="index.php" method="get">
 
-                    <label for="pwdlen"></label>
-                    <input type="number" name="pwdlen" id="pwdlen" placeholder="Lunghezza password" min="8" max="32">
+                    <div class="row my-2">
+                        <div class="col-8">
+                            <label for="pwdlen">Lunghezza della password</label>
 
-                    <div id="pulsantiera" class="mt-2">
-                        <button type="submit" class="btn btn-primary">Invia</button>
-                        <button type="reset" class="btn btn-secondary">Pulisci form</button>
+                        </div>
+                        <div class="col-4">
+                            <input type="number" name="pwdlen" id="pwdlen" placeholder="Lunghezza password" min="8" max="32" value="8">
+                        </div>
+                    </div>
+
+                    <div class="row my-2">
+                        <div class="col-8">
+                            <label for="repetition">Consenti ripetizioni di uno o più caratteri</label>
+
+                        </div>
+                        <div class="col-4">
+                            <select name="repetition" class="form-select" aria-label="Seleziona se sarà possibile utilizzare più volte un carattere">
+                                <option selected value="1">Si</option>
+                                <option value="0">No</option>
+                            </select>
+                        </div>
+                    </div>
+
+                    <div class="row my-2">
+                        <div class="col-8">
+                        <label>Insiemi di caratteri da utilizzare</label>
+                        </div>
+                        <div class="col-4">
+
+                            <div class="form-check">
+                                <input class="form-check-input" type="checkbox" name="subset_azlower" value="1" checked>
+                                <label class="form-check-label" for="subset_azlower">
+                                    Lettere a-z
+                                </label>
+                            </div>
+                            
+                            <div class="form-check">
+                            <input class="form-check-input" type="checkbox" name="subset_azupper" value="1" checked>
+                            <label class="form-check-label" for="subset_azupper">
+                                Lettere A-Z
+                            </label>
+                            </div>
+                            
+                            <div class="form-check">
+                            <input class="form-check-input" type="checkbox" name="subset_numbers" value="1">
+                            <label class="form-check-label" for="subset_numbers">
+                                Numeri
+                            </label>
+                            </div>
+                            
+                            <div class="form-check">
+                            <input class="form-check-input" type="checkbox" name="subset_symbols" value="1">
+                            <label class="form-check-label" for="subset_symbols">
+                                Simboli
+                            </label>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row my-2">
+                        <div class="col-12">
+                            <button type="submit" class="btn btn-primary">Invia</button>
+                            <button type="reset" class="btn btn-secondary">Pulisci form</button>
+                        </div>
                     </div>
 
                 </form>
